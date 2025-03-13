a89159414c2c793e74950ebd99a3071968a4a708
marialuisarg <marialuisa@ice.ufjf.br>
2025-03-13 20:22:31 -0300

    versão final

--- T2/interpreter.rkt
+++ T2/interpreter.rkt
@@ -8,73 +8,17 @@
 
 (provide eval-query)
 
-;; ---------------------------
-;; Representação do Programa (Base de Conhecimento)
-;; ---------------------------
+;; Representação do programa (base de conhecimento)
 (define knowledge-base '())
 
+;; Carrega o programa (fatos e regras) na base de conhecimento
 (define (load-program prog)
   (set! knowledge-base (if (list? prog) prog (list prog))))
 
-;; ---------------------------
-;; Função auxiliar: converte uma consulta (que pode ser um improper list) em uma lista de goals
-;; ---------------------------
-(define (goals->list q)
-  (cond
-    [(null? q) '()]
-    [(pair? q) (cons (car q) (goals->list (cdr q)))]
-    [else (list q)]))
+;; ---------------------------------------------------------
+;; FUNÇÃO PRINCIPAL: recebe e avalia a consulta / retorna resultado(s)
+;; ---------------------------------------------------------
 
-;; ---------------------------
-;; Funções de Freshening (Renomeação) para Cláusulas (regras)
-;; ---------------------------
-(define (rename-term term env)
-  (cond
-    [(ast:var? term)
-     (let ([binding (assoc term env)])
-       (if binding
-           (values (cdr binding) env)
-           (let ([new-var (ast:var (gensym (ast:var-name term)))])
-             (values new-var (cons (cons term new-var) env)))))]
-    [(ast:unknow-var? term)
-     (let ([new-var (ast:var (gensym '_))])
-       (values new-var env))]
-    [(ast:atom? term) (values term env)]
-    [(ast:functor? term)
-     (let-values ([(new-args new-env) (rename-term-list (ast:functor-args term) env)])
-       (values (ast:functor (ast:functor-name term) new-args) new-env))]
-    [else (values term env)]))
-
-(define (rename-term-list terms env)
-  (cond
-    [(null? terms) 
-     (values '() env)]
-    [(pair? terms)
-     (call-with-values
-      (lambda () (rename-term (car terms) env))
-      (lambda (new-term env1)
-        (call-with-values
-         (lambda () (rename-term-list (cdr terms) env1))
-         (lambda (new-terms env2)
-           (values (cons new-term new-terms) env2)))))]
-    [else
-     (call-with-values
-      (lambda () (rename-term terms env))
-      (lambda (new-term env1)
-        (values (list new-term) env1)))]))
-
-(define (rename-clause clause)
-  (call-with-values
-   (lambda () (rename-term (clause-head clause) '()))
-   (lambda (new-head env1)
-     (call-with-values
-      (lambda () (rename-term-list (clause-body clause) env1))
-      (lambda (new-body env2)
-        (ast:clause new-head new-body))))))
-
-;; ---------------------------
-;; Função Principal: Avaliar Consulta
-;; ---------------------------
 (define (eval-query program query)
   (load-program program)
   (let* ([goals (if (pair? query)
@@ -95,14 +39,11 @@
                     results)
           #'(void)))))
 
-;; ---------------------------
-;; Resolução de Consultas (Goals)
-;; ---------------------------
-(define (resolve-query query env)
-  (if (null? knowledge-base)
-      '()
-      (search-rules query env knowledge-base)))
+;; ---------------------------------------------------------
+;; RESOLUÇÃO DAS CONSULTAS
+;; ---------------------------------------------------------
 
+;; resolve cada subgoal
 (define (resolve-query-body goals env)
   (cond
     [(null? goals) (list env)]
@@ -115,9 +56,7 @@
                    (resolve-query-body (cdr goals) env1))
                  (search-rules (car goals) env knowledge-base))]))
 
-;; ---------------------------
-;; Busca por Fatos e Regras
-;; ---------------------------
+;; busca por fatos e regras na base de conhecimento
 (define (search-rules query env rules)
   (if (null? rules)
       '()
@@ -128,25 +67,24 @@
         (append (if unif (resolve-query-body body-goals unif) '()) 
                 (search-rules query env (cdr rules))))))
 
-;; ---------------------------
-;; Unificação de Termos
-;; ---------------------------
+;; substitui variável por seu valor no ambiente (env)
 (define (walk term env)
   (if (and (ast:var? term) (assoc term env))
       (walk (cdr (assoc term env)) env)
       term))
 
+;; UNIFICAÇÃO
 (define (unify term1 term2 env)
   (let* ([t1 (walk term1 env)]
          [t2 (walk term2 env)])
     (cond
-      [(equal? t1 t2) env]
-      [(ast:var? t1) (bind-variable t1 t2 env)]
-      [(ast:var? t2) (bind-variable t2 t1 env)]
-      [(and (ast:functor? t1) (ast:functor? t2))
+      [(equal? t1 t2) env]                          ; termos iguais -> retorna env
+      [(ast:var? t1) (bind-variable t1 t2 env)]     ; um termo var -> associa ao outro termo
+      [(ast:var? t2) (bind-variable t2 t1 env)]     
+      [(and (ast:functor? t1) (ast:functor? t2))    
        (if (equal? (ast:functor-name t1) (ast:functor-name t2))
            (unify-args (ast:functor-args t1) (ast:functor-args t2) env)
-           #f)]
+           #f)]                                     ; functores -> compara nomes e unifica
       [else #f])))
 
 (define (unify-args args1 args2 env)
@@ -161,55 +99,14 @@
              #f))]
       [else #f])))
 
-;; ---------------------------
-;; Associação de Variáveis no Ambiente
-;; ---------------------------
-(define (bind-variable var value env)
-  (if (assoc var env)
-      env
-      (cons (cons var value) env)))
-
-;; ---------------------------
-;; Impressão do Ambiente (Melhorada)
-;; ---------------------------
-(define (display-env env)
-  (for-each
-   (lambda (binding)
-     (let* ([var (car binding)]
-            [value (cdr binding)]
-            ;; Extrai nomes base das variáveis
-            [var-name (extract-name var)]
-            [value-name (extract-name value)])
-       (display (format "~a = ~a\n" var-name value-name))))
-   env))
-
-(define (extract-name term)
-  (cond
-    [(ast:var? term)
-     ;; Converte para string se for símbolo ou mantém a string original
-     (let* ([name (ast:var-name term)]
-            [name-str (if (symbol? name) 
-                          (symbol->string name) 
-                          name)]
-            [base-name (car (regexp-match #px"^([A-Za-z]+)" name-str))])
-       (string->symbol base-name))]
-    [(ast:atom? term) (ast:atom-name term)]
-    [else term]))
-
-
-;; ---------------------------
-;; Operação de Desigualdade
-;; ---------------------------
+;; DESIGUALDADE
 (define (check-diff env term1 term2)
-  ;(displayln (format "[DEBUG] Verificando desigualdade: ~a ≠ ~a" term1 term2))
   (let* ([val1 (walk term1 env)]
          [val2 (walk term2 env)])
-    ;(displayln (format "[DEBUG] Valores após substituição completa: ~a ≠ ~a" val1 val2))
     (not (terms-equal? val1 val2))))
 
 (define (terms-equal? t1 t2)
-  ;(displayln (format "[DEBUG] Comparando estruturalmente: ~a vs ~a" t1 t2))
-  (let ([t1 (walk t1 '())] ; Força caminhada completa sem ambiente (valores finais)
+  (let ([t1 (walk t1 '())]            ; final -> força caminhada completa sem env
         [t2 (walk t2 '())])
     (cond
       [(and (ast:var? t1) (ast:var? t2)) (eq? t1 t2)]
@@ -220,9 +117,96 @@
                     (ast:functor-args t2)))]
       [else (equal? t1 t2)])))
 
-;; ---------------------------
-;; Predicados Auxiliares
-;; ---------------------------
+;; ---------------------------------------------------------
+;; Funções Auxiliares
+;; ---------------------------------------------------------
+
+;; (AUX) Converte a consulta em uma lista de objetivos
+(define (goals->list q)
+  (cond
+    [(null? q) '()]
+    [(pair? q) (cons (car q) (goals->list (cdr q)))]
+    [else (list q)]))
+
+;; (AUX) Associa variáveis no ambiente
+(define (bind-variable var value env)
+  (if (assoc var env)
+      env
+      (cons (cons var value) env)))
+
+;; (AUX) Renomeia as variáveis em cláusulas quando necessário
+(define (rename-term term env)
+  (cond
+    [(ast:var? term)
+     (let ([binding (assoc term env)])
+       (if binding
+           (values (cdr binding) env)
+           (let ([new-var (ast:var (gensym (ast:var-name term)))])
+             (values new-var (cons (cons term new-var) env)))))]
+    [(ast:unknow-var? term)
+     (let ([new-var (ast:var (gensym '_))])
+       (values new-var env))]
+    [(ast:atom? term) (values term env)]
+    [(ast:functor? term)
+     (let-values ([(new-args new-env) (rename-term-list (ast:functor-args term) env)])
+       (values (ast:functor (ast:functor-name term) new-args) new-env))]
+    [else (values term env)]))
+
+;; (AUX) Renomeia lista de termos
+(define (rename-term-list terms env)
+  (cond
+    [(null? terms) 
+     (values '() env)]
+    [(pair? terms)
+     (call-with-values
+      (lambda () (rename-term (car terms) env))
+      (lambda (new-term env1)
+        (call-with-values
+         (lambda () (rename-term-list (cdr terms) env1))
+         (lambda (new-terms env2)
+           (values (cons new-term new-terms) env2)))))]
+    [else
+     (call-with-values
+      (lambda () (rename-term terms env))
+      (lambda (new-term env1)
+        (values (list new-term) env1)))]))
+
+;; (AUX) Renomeia cabeça e corpo das cláusulas
+(define (rename-clause clause)
+  (call-with-values
+   (lambda () (rename-term (clause-head clause) '()))
+   (lambda (new-head env1)
+     (call-with-values
+      (lambda () (rename-term-list (clause-body clause) env1))
+      (lambda (new-body env2)
+        (ast:clause new-head new-body))))))
+
+;; (AUX) Imprime o ambiente (env)
+(define (display-env env)
+  (for-each
+   (lambda (binding)
+     (let* ([var (car binding)]
+            [value (cdr binding)]
+            ;; extrai nomes base das variáveis
+            [var-name (extract-name var)]
+            [value-name (extract-name value)])
+       (display (format "~a = ~a\n" var-name value-name))))
+   env))
+
+(define (extract-name term)
+  (cond
+    [(ast:var? term)
+     ;; converte para string se for símbolo ou mantém a string original
+     (let* ([name (ast:var-name term)]
+            [name-str (if (symbol? name) 
+                          (symbol->string name) 
+                          name)]
+            [base-name (car (regexp-match #px"^([A-Za-z]+)" name-str))])
+       (string->symbol base-name))]
+    [(ast:atom? term) (ast:atom-name term)]
+    [else term]))
+
+;; (AUX) Predicados
 (require (prefix-in ast: dcc019/exercise/logic/ast))
 (define (var? x) (ast:var? x))
 (define (functor? x) (ast:functor? x))
